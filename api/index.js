67546a72a0da086fdf90e08ae5925b9c3d1ae20c
adipairunan16 <adi.pairunan16@guru.smk.belajar.id>
2026-05-18 22:36:48 +0800

    coba terus

--- api/index.js
+++ api/index.js
@@ -1,23 +1,9 @@
-const express = require('express')
 const mongoose = require('mongoose')
-const cors = require('cors')
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.get('/api/students', async(req,res)=>{
-  const students = await Student.find()
-  res.json(students)
-})
-// CONNECT MONGODB
 mongoose.connect(process.env.MONGO_URI, {
   dbName: 'db_kelulusan'
 })
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err))
 
-// SCHEMA
 const StudentSchema = new mongoose.Schema({
   nisn:String,
   nama:String,
@@ -25,71 +11,38 @@ const StudentSchema = new mongoose.Schema({
   status:String
 })
 
-const AdminSchema = new mongoose.Schema({
-  username:String,
-  password:String
-})
-
 const Student = mongoose.model(
   'students',
   StudentSchema
 )
 
-const Admin = mongoose.model(
-  'admin',
-  AdminSchema
-)
+module.exports = async (req, res) => {
 
-// ROOT
-app.get('/api', (req,res)=>{
-  res.send('Backend hidup')
-})
+  // ROOT
+  if(req.url === '/api'){
 
-// GET ALL STUDENTS
-app.get('/api/students', async(req,res)=>{
-
-  const students = await Student.find()
-
-  res.json(students)
-})
-
-// GET BY NISN
-app.get('/api/students/:nisn', async(req,res)=>{
-
-  const student = await Student.findOne({
-    nisn:req.params.nisn
-  })
-
-  if(!student){
-
-    return res.status(404).json({
-      message:'Data tidak ditemukan'
-    })
+    return res.status(200).send('API hidup')
   }
 
-  res.json(student)
-})
+  // GET ALL STUDENTS
+  if(req.url === '/api/students'){
 
-// LOGIN
-app.post('/api/login', async(req,res)=>{
+    try {
 
-  const {username,password} = req.body
+      const students = await Student.find()
 
-  const admin = await Admin.findOne({
-    username,
-    password
-  })
+      return res.status(200).json(students)
 
-  if(!admin){
+    } catch(err){
 
-    return res.status(401).json({
-      message:'Username atau password salah'
-    })
+      return res.status(500).json({
+        error: err.message
+      })
+    }
   }
 
-  res.json({
-    message:'Login berhasil'
+  // NOT FOUND
+  return res.status(404).json({
+    message:'Route tidak ditemukan'
   })
-})
-
-module.exports = app
+}
